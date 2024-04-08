e9cce3564df9a40593880e6d31b74bde472a6146
Adrian Nieves de la Cruz <ingadriannievescruz@gmail.com>
2024-04-09 00:33:46 +0200

    [ADD] Animation to general-card

--- src/app/pages/contenido/contenido.page.ts
+++ src/app/pages/contenido/contenido.page.ts
@@ -2,33 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { NavController, Platform } from '@ionic/angular';
 
 @Component({
-  selector: 'app-contenido',
-  templateUrl: './contenido.page.html',
-  styleUrls: ['./contenido.page.scss'],
+    selector: 'app-contenido',
+    templateUrl: './contenido.page.html',
+    styleUrls: ['./contenido.page.scss'],
 })
 export class ContenidoPage implements OnInit {
-  options: string[] = ['Electrostática', 'Corriente', 'Magnetismo'];
+    options: string[] = ['Electrostática', 'Corriente', 'Magnetismo'];
 
-  constructor(public navCtrl: NavController, private platform: Platform) {}
+    constructor(public navCtrl: NavController, private platform: Platform) {}
 
-  ngOnInit() {
-    this.platform.backButton.subscribeWithPriority(10, () => {
-      this.navCtrl.navigateRoot('/home', {
-        animated: true,
-        animationDirection: 'back',
-      });
-    });
-  }
-  routeTopage(element: number) {
-    if (element === 2) {
-    } else {
-      this.navCtrl.navigateRoot(
-        `/${this.options[element].toLocaleLowerCase()}`,
-        {
-          animated: true,
-          animationDirection: 'forward',
-        }
-      );
+    ngOnInit() {
+        this.platform.backButton.subscribeWithPriority(10, () => {
+            this.navCtrl.navigateRoot('/home', {
+                animated: true,
+                animationDirection: 'back',
+            });
+        });
     }
-  }
+    routeTopage(element: number) {
+        setTimeout(() => {
+            if (element === 2) {
+            } else {
+                this.navCtrl.navigateRoot(`/${this.options[element].toLocaleLowerCase()}`, {
+                    animated: true,
+                    animationDirection: 'forward',
+                });
+            }
+        }, 300);
+    }
 }
