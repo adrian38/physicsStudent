4de0d8b09cf54af856b8c17cfb4ae502f5495cae
Adrian Nieves de la Cruz <ingadriannievescruz@gmail.com>
2024-04-09 15:45:28 +0200

    Minor change

--- src/app/pages/contenido/contenido.page.ts
+++ src/app/pages/contenido/contenido.page.ts
@@ -9,33 +9,17 @@ import { MenuController, NavController, Platform } from '@ionic/angular';
 export class ContenidoPage implements OnInit {
     options: string[] = ['Electrostática', 'Corriente', 'Magnetismo'];
 
-  constructor(public navCtrl: NavController, 
-              private platform: Platform,
-              ) {}
+    constructor(public navCtrl: NavController, private platform: Platform) {}
 
-  ngOnInit() {
-    this.platform.backButton.subscribeWithPriority(10, () => {
-      this.navCtrl.navigateRoot('/home', {
-        animated: true,
-        animationDirection: 'back',
-      });
-    });
-  }
-
-  
-
-
-  routeTopage(element: number) {
-    if (element === 2) {
-    } else {
-      this.navCtrl.navigateRoot(
-        `/${this.options[element].toLocaleLowerCase()}`,
-        {
-          animated: true,
-          animationDirection: 'forward',
-        }
-      );
+    ngOnInit() {
+        this.platform.backButton.subscribeWithPriority(10, () => {
+            this.navCtrl.navigateRoot('/home', {
+                animated: true,
+                animationDirection: 'back',
+            });
+        });
     }
+
     routeTopage(element: number) {
         setTimeout(() => {
             if (element === 2) {
